7258e68b87577689e4c981d6286c7daeab8494d7
wills <wills@localhost>
2016-04-06 15:57:54 -0400

    Commented Code

--- TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
+++ TestDogBLC/src/edu/saintjoe/cs/brianc/testdogblc/TestDogBLC.java
@@ -1,33 +1,39 @@
 package edu.saintjoe.cs.brianc.testdogblc;
 
 public class TestDogBLC {
-
+// Create a new private String called name 
 	    private String name;
-	    
+	   
+	    // Create new private int called weight 
 	    private int weight;
-	    	    
+	    
+	    // Set the name Void So you can add a name later
 	    public void setName(String newName) {
 	    	name = newName;
 	    }
 	    
-	    public void setWeight(int newWeight) {
+	   // Set weight void so you can set a numbered weight 
+	   public void setWeight(int newWeight) {
 	    	weight = newWeight;
 	    }
 	    
-
+	   // Getting the String from a public source and returning it  
 	    public String getName() {
 	    	return name;
 	    }
 	    
+	    // Getting the public weight and returning it 
 	    public int getWeight() {
 	    	return weight;
 	    }
 	    
+	    // making the Bark void 
 	    public void bark() {
 	    
 	    	int pointless = 0;
 	    	
 	    	
+	    	//  If/ else statements setting the bark noisr based off the weight of the dog
 	    	if (weight > 60 ) {
 	          System.out.println("Woof!! Woof!! says " + name + " that weighs " + weight);
 	    	} else if (weight > 14 ) {
@@ -36,6 +42,7 @@ public class TestDogBLC {
 	    		System.out.println("Yip!! Yip!! says " + name + " that weighs " + weight);
 	    }
 	    
+	    //  Setting both the string newName and int newWeight at the same time 
 	    public int setBoth(String newName, int newWeight) {
 	    	
 	    	int oldWeight = weight;
@@ -45,7 +52,7 @@ public class TestDogBLC {
 	    	return oldWeight;
 	    }
 	    	
-
+//  Leaving the eating procedure to void so you can set them to an int or String name later 
 	    public void eat() { }
 	    
 	    public void chaseCat() { }
